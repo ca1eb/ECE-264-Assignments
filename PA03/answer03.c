0e9328c7f02bf601483f819768dd193aec17025b
Caleb Paul Niemann <niemann@ecegrid-thin1.ecn.purdue.edu>
2014-09-15 23:32:48 -0400

    partial explode

--- PA03/answer03.c
+++ PA03/answer03.c
@@ -24,3 +24,42 @@ char * strcat_ex(char * * dest, int * n, const char * src)
   return (char*) dest;
 }
 
+char * * explode(const char * str, const char * delims, int * arrLen)
+{
+  //LOCAL DECLARATIONS
+  int counter;
+  int ind;
+  
+  //EXECUTABLE STATEMENTS
+  int n = 0;
+  for (counter = 0; counter < strlen(str); counter++)
+    {
+      if (strchr(delims,str[counter]) != NULL)
+	{
+	  n++;
+	}
+    }
+  printf("%d\n", n);
+  char * * strArr = malloc((n + 1) * sizeof(char *));
+  
+  int arrInd = 0; // this is the next position where we'll create a string
+  int last = 0; // 1 + the last index we saw a delimiter. Init to 0.
+  for (ind = 0; ind <= strlen(str); ind++) //POTENTIAL CHANGE TO <=
+    {
+      printf("%c\n",str[ind]);
+      if (strchr(delims,str[ind]) != NULL)
+	{
+	  char * tmp = 
+	  memcpy(*strArr,);
+	  *strArr[arrInd] = *tmp;
+	  last = ind + 1;  
+	  arrInd++;
+	}
+    }
+  char * end = malloc(sizeof(char) * (strlen(str) - last));
+  *strArr[n] = *end;
+  
+  *arrLen = n;
+
+  return strArr;
+}
--- PA03/pa03.c
+++ PA03/pa03.c
@@ -6,7 +6,7 @@
 
 int main(int argc, char * * argv)
 {
-  //LOCAL DECLARATIONS
+  //strcat_ex
   char * str1 = malloc(5 * sizeof(char));
   strcpy(str1,"one");  
   char * str2 = malloc(5 * sizeof(char));
@@ -14,11 +14,19 @@ int main(int argc, char * * argv)
   int * n;
   int nvalue = 10;
   n = &nvalue;
-
-  //EXECUTABLE STATEMENTS
   strcat_ex(&str1,n,str2);
-
   printf("%s\n", str1);
+  
+  //
+  char * str3 = malloc(15 * sizeof(char));
+  strcpy(str3,"this is three");
+  char * str4 = malloc(13 * sizeof(char));
+  strcpy(str4,"three i say");
+  
+  int len;
+  char * * strArr = explode("The\nTuring test", " \t\v\n\r\f", &len);
+  printf("%s\n %s\n %s\n", strArr[0], strArr[1], strArr[2]);  
+
   return EXIT_SUCCESS;
 }
 
