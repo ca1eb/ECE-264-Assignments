af38343f07fea8672ccac3ce29e5a74811500c64
Caleb Paul Niemann <niemann@ecegrid-thin3.ecn.purdue.edu>
2014-09-19 20:24:35 -0400

    completed assignment

--- PA03/answer03.c
+++ PA03/answer03.c
@@ -5,13 +5,27 @@
 char * strcat_ex(char * * dest, int * n, const char * src)
 {
   //LOCAL DECLARATIONS
+  int destLen;
 
   //EXECUTABLE STATEMENTS
   if (*dest == NULL || (*n < (strlen(*dest) + strlen(src) + 1))) 
     {
-      char * buffer= malloc(1 + 2 * (strlen(*dest) + strlen(src)) * sizeof(char));
-      *n = (1 + 2 * (strlen(*dest) + strlen(src)));
-      strcpy(buffer,*dest);
+      if (*dest != NULL)
+	{
+          destLen = strlen(*dest);
+	}
+      else
+	{
+	  destLen = 0;
+	}
+
+      char * buffer = malloc((1 + 2 * (destLen + strlen(src))) * sizeof(char));
+      *n = (1 + 2 * (destLen + strlen(src)));
+      *buffer = '\0';
+      if (*dest != NULL)
+	{
+	  strcpy(buffer,*dest);
+        }
       free(*dest);
       *dest = &buffer[0];
       strcat(*dest,src);
@@ -21,46 +35,140 @@ char * strcat_ex(char * * dest, int * n, const char * src)
       strcat(*dest,src);
     }
 
-  return (char*) dest;
+  return *dest;
 }
+
+char * make_substr(const char * s, int start_ind, int len)
+{
+  //LOCAL DECLARATIONS
+  char * buffer = malloc(sizeof(char) * (len + 1));
+  s = s + start_ind;
+  strncpy(buffer,s,len);
+  buffer[len] = '\0';
+
+  return buffer;
+}
+
 
 char * * explode(const char * str, const char * delims, int * arrLen)
 {
-  //LOCAL DECLARATIONS
-  int counter;
-  int ind;
-  
-  //EXECUTABLE STATEMENTS
   int n = 0;
+  int ind;
+  int counter;
+  int arrInd = 0;
+
+  for (ind = 0; ind < strlen(str); ind++)
+    {
+      if (strchr(delims,str[ind]) != NULL)
+        {
+          n++;
+        }
+    }
+ 
+  *arrLen = n + 1;  
+  
+  int last = 0;
+  char * * arr = malloc(sizeof(char*) * (*arrLen));
+  
   for (counter = 0; counter < strlen(str); counter++)
     {
       if (strchr(delims,str[counter]) != NULL)
 	{
-	  n++;
-	}
-    }
-  printf("%d\n", n);
-  char * * strArr = malloc((n + 1) * sizeof(char *));
-  
-  int arrInd = 0; // this is the next position where we'll create a string
-  int last = 0; // 1 + the last index we saw a delimiter. Init to 0.
-  for (ind = 0; ind < strlen(str); ind++) //POTENTIAL CHANGE TO <=
-    {
-      printf("%c\n",str[ind]);
-      if (strchr(delims,str[ind]) != NULL)
-	{
-	  char * tmp = malloc(sizeof(char) * (ind + 1));
-	  int bits = ind - last;
-	  memcpy(*strArr,tmp,bits);
-	  *strArr[arrInd] = *tmp;
-	  last = ind + 1;  
+	  arr[arrInd] = malloc(sizeof(char) * (counter - last + 1));
+	  arr[arrInd][0] = '\0';
+	  memcpy(arr[arrInd],str + last, counter - last);
+	  arr[arrInd][counter - last] = '\0';
+	  last = counter + 1;
 	  arrInd++;
 	}
     }
-  //char * end = malloc(sizeof(char) * (strlen(str) - last));
-  //*strArr[n] = *end;
   
-  *arrLen = n;
+  arr[arrInd] = malloc(sizeof(char) * (counter - last + 1));
+  arr[arrInd][0] = '\0';
+  memcpy(arr[arrInd],str + last, counter - last);
+  arr[arrInd][counter - last] = '\0';
 
-  return strArr;
+  return arr;
 }
+
+char * implode(char * * strArr, int len, const char * glue)
+{
+  //LOCAL DECLARATIONS
+  int ind;
+  int arrLen;
+  int glueLen = strlen(glue);
+  char * dest = NULL;
+
+  //EXECUTABLE STATEMENTS
+  for (ind = 0; ind < len; ind++)
+    {
+      arrLen = strlen(strArr[ind]);
+      strcat_ex(&dest,&arrLen,strArr[ind]);
+      if (ind != (len - 1))
+	{
+	  strcat_ex(&dest,&glueLen,glue);  // glue doesn't go at the very end
+	}
+    }
+
+  return dest;
+}
+
+int cmpfuncc(const void * a, const void * b)
+{  //or *(char*)a
+  const char * ptr1 = (const char *) a;
+  const char * ptr2 = (const char *) b;
+  int val1 = (int) *ptr1;
+  int val2 = (int) *ptr2;
+  if (val1 < val2)
+    {
+      return -1;
+    }
+  if (val1 == val2)
+    {
+      return 0;
+    }
+
+  return 1;
+}
+
+int cmpfuncs(const void * a, const void * b)
+{
+  const char * ptr1 = * (char **) a;
+  const char * ptr2 = * (char **) b;
+
+  return (strcmp(ptr1,ptr2));
+}
+
+void sortStringArray(char * * arrString, int len)
+{
+  //LOCAL DECLARATIONS
+  
+  //EXECUTABLE STATEMENTS
+  qsort(arrString,len,sizeof(char*),cmpfuncs);
+
+  return;
+}
+
+void sortStringCharacters(char * str)
+{
+  qsort(str,strlen(str),sizeof(char),cmpfuncc);
+  
+  return;
+}
+
+void destroyStringArray(char * * strArr, int len)
+{
+  int ind;
+  
+  for (ind = 0; ind < len; ind++)
+    {
+      free(strArr[ind]);
+    }
+
+  free(strArr);	
+  strArr = NULL;
+
+  return;
+}
+
+
--- PA03/pa03.c
+++ PA03/pa03.c
@@ -3,30 +3,25 @@
 #include<stdlib.h>
 #include"answer03.h"
 
-
 int main(int argc, char * * argv)
 {
-  //strcat_ex
-  char * str1 = malloc(5 * sizeof(char));
-  strcpy(str1,"one");  
-  char * str2 = malloc(5 * sizeof(char));
-  strcpy(str2,"two");
-  int * n;
-  int nvalue = 10;
-  n = &nvalue;
-  strcat_ex(&str1,n,str2);
-  printf("%s\n", str1);
-  
-  //
-  char * str3 = malloc(15 * sizeof(char));
-  strcpy(str3,"this is three");
-  char * str4 = malloc(13 * sizeof(char));
-  strcpy(str4,"three i say");
-  
-  int len;
-  char * * strArr = explode("The\nTuring test", " \t\v\n\r\f", &len);
-  printf("%s\n %s\n %s\n", strArr[0], strArr[1], strArr[2]);  
+  //char * arr = malloc(sizeof(char) * 18);
+  //strcpy(arr,"a big long string");
 
+  //char * arr2 = make_substr(arr,2,3);
+  //printf("string = %s\n", arr2);
+ 
   return EXIT_SUCCESS;
 }
+/*
+char * make_substr(const char * s, int start_ind, int len)
+{
+  //LOCAL DECLARATIONS                                                                                                                                                                                                                       
+  char * buffer = malloc(sizeof(char) * (len + 1));
+  memcpy(buffer,s,len);
+  buffer[len + 1] = '\0';
+
+  return buffer;
+}
+*/
 
