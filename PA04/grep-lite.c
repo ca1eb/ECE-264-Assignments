15047d241c835a33078a41e63ca7df41733aa808
Caleb Paul Niemann <niemann@ecegrid-thin1.ecn.purdue.edu>
2014-10-01 22:27:02 -0400

    complete

--- PA04/grep-lite.c
+++ PA04/grep-lite.c
@@ -2,61 +2,198 @@
 #include<stdlib.h>
 #include<string.h>
 
+#define TRUE 1
+#define FALSE 0
+
 int main(int argc, char * * argv)
 {
   //LOCAL DECLARATIONS
   int ind;
+  int helpSwitch = FALSE;
+  int qSwitch = FALSE;
+  int vSwitch = FALSE;
+  int nSwitch = FALSE;
+  int noSwitch = FALSE;
+  int counter = 1;
+  int nCase = FALSE;
+  int vCase = FALSE;
+  int noCase = FALSE;
+  int errSwitch = FALSE;
 
   //EXECUTABLE STATEMENTS
-  for (ind = 1; ind < argc; ind++)
+  for (ind = 1; ind < argc - 1; ++ind) 
     {
-      if (strcmp(argv[ind],"--help") == 0)
-        {
-          printf("Usage: grep-lite [OPTION]... PATTERN\n");
-          printf("Search for PATTERN in standard input. PATTERN is a\n");
-	  printf("string. grep-lite will search standard input line by\n");
-	  printf("line, and (by default) print out those lines which\n");
-	  printf("contain pattern as a substring.\n\n");
-          printf("  -v, --invert-match     print non-matching lines\n");
-          printf("  -n, --line-number      print line numbers with output\n");
-          printf("  -q, --quiet            suppress all output\n\n");
-          printf("Exit status is 0 if any line is selected, 1 otherwise;\n");
-          printf("if any error occurs, then the exit status is 2.\n");                                                                               
-          
-	  return 1;
-        }
+      if (strcmp(argv[ind], "--help") == 0)
+	{
+	  helpSwitch = TRUE;
+	  //printf("help\n");
+	}
+      else if (strcmp(argv[ind], "-q") == 0 || strcmp(argv[ind], "--quiet") == 0)
+	{
+	  qSwitch = TRUE;
+	  //printf("q\n");
+	}
+      else if (strcmp(argv[ind], "-v") == 0 || strcmp(argv[ind], "--invert-match") == 0)
+	{
+	  vSwitch = TRUE;
+	  //printf("v\n");
+	}
+      else if (strcmp(argv[ind], "-n") == 0 || strcmp(argv[ind],"--line-number") == 0) 
+	{
+	  nSwitch = TRUE; 
+	  //printf("n\n");
+	}
+    }
+
+  if (!strcmp(argv[1],"-n") && !strcmp(argv[1],"-q") && !strcmp(argv[1],"-v") && !strcmp(argv[1],"--quiet") && !strcmp(argv[1],"--invert-match") && !strcmp(argv[1],"--line-number"))
+    {
+      errSwitch = TRUE;
     }
   
-  if ((strcmp(argv[1],"-v") == 0 || strcmp(argv[1],"-q") == 0 || strcmp(argv[1],"-n")== 0 || strcmp(argv[1],"--quiet") == 0 || strcmp(argv[1],"--line-number") == 0 || strcmp(argv[1],"--invert-match") == 0) && (argv[argc - 1][0] != '-'))
+  if (!nSwitch && !vSwitch && !qSwitch && !helpSwitch)
+    {
+      noSwitch = TRUE;
+      //printf("no\n");
+    }
+  //printf("%d\n", noSwitch);
+
+  if (errSwitch)
+    {
+      if (argv[argc - 1][0] == '-')                                                                                                         
+        {                                                                                                                                    
+          fprintf(stderr, "Last argument must not end in a '-'\n");                                                                          
+        } 
+      else                                                                                                                                  
+        {                                                                                                                                    
+          fprintf(stderr, "Command Line Argument Is Not Valid\n");                                                           
+	} 
+      
+      return 2;
+    }
+
+  if (helpSwitch)
+    {
+      for (ind = 1; ind < argc; ind++)
+	{
+	  if (strcmp(argv[ind],"--help") == 0)
+	    {
+	      printf("Usage: grep-lite [OPTION]... PATTERN\n");
+	      printf("Search for PATTERN in standard input. PATTERN is a\n");
+	      printf("string. grep-lite will search standard input line by\n");
+	      printf("line, and (by default) print out those lines which\n");
+	      printf("contain pattern as a substring.\n\n");
+	      printf("  -v, --invert-match     print non-matching lines\n");
+	      printf("  -n, --line-number      print line numbers with output\n");
+	      printf("  -q, --quiet            suppress all output\n\n");
+	      printf("Exit status is 0 if any line is selected, 1 otherwise;\n");
+	      printf("if any error occurs, then the exit status is 2.\n");                                                              
+
+	      return 1;
+	    }
+	}
+    }
+  else if (noSwitch || qSwitch)
+    {
+      FILE * currentFile;                                                                                                                    
+      currentFile = stdin;                                                                                                                
+      char arr[2000];  
+      while (fgets(arr,2000,currentFile))
+	{
+	  //fgets(arr,2000,currentFile);  
+	  if (strstr(arr,argv[argc - 1]) != NULL)                                                           
+	    {        
+	      noCase = TRUE;
+	      if (!qSwitch)
+		{
+		  printf("%s", arr);
+		}
+	    }
+	}
+      if (noCase)
+        {
+          return 0;
+        }
+      else
+        {
+          return 1;
+        }
+    }
+  else if (vSwitch && nSwitch)
     {
       FILE * currentFile;
       currentFile = stdin;
       char arr[2000];
+      while(fgets(arr,2000,currentFile))
+        {
+          if (strstr(arr,argv[argc - 1]) == NULL)
+            {
+              vCase = TRUE;
+	      if (!qSwitch)
+		{
+		  printf("%d:%s", counter, arr);
+		}
+            }
+	  counter++;
+        }
+      if (nCase)
+        {
+          return 1;
+        }
+      else
+        {
+          return 0;
+        }
+    }
       
-      while (feof(currentFile) != 0)
+  else if (vSwitch)
+    {
+      FILE * currentFile;
+      currentFile = stdin;
+      char arr[2000];
+      while(fgets(arr,2000,currentFile))
 	{
-	  fgets(arr, 2000, currentFile);
-	  if (strstr(arr,argv[argc-1]) == 0)
+	  if (strstr(arr,argv[argc - 1]) == NULL)
 	    {
-	      //fprintf
+	      vCase = TRUE;
+	      if (!qSwitch)
+		{
+		  printf("%s",arr);
+		}
 	    }
 	}
-
-      return 1;
-    }
-  else
-    {
-      if (argv[argc - 1][0] == '-')
+      if (nCase)
 	{
-	  fprintf(stderr, "Last argument must not end in a '-'\n");
-	}
+          return 1;
+        }
       else
 	{
-	  fprintf(stderr, "Command Line Argument Is Not Valid\n");
+          return 0;
+        }
+    }
+  else if (nSwitch)
+    {
+      FILE * currentFile;
+      currentFile = stdin;
+      char arr[2000];
+      while (fgets(arr,2000,currentFile))
+        {
+          //fgets(arr,2000,currentFile);                                                                                  
+          if (strstr(arr,argv[argc - 1]) != NULL)
+            {
+	      nCase = TRUE;
+	      if (!qSwitch)
+		{
+		  printf("%d:%s", counter, arr);
+		}
+            }
+	  counter++;
 	}
-
-      return 2;
+      
+      if (nCase)
+	{
+	  return 0;
+	}
     }
 
-  return EXIT_SUCCESS;
+  return 1;
 }
