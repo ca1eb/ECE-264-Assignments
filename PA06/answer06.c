a65ab7a97300360b084067bdf5c8b99d15bc26bf
Caleb Paul Niemann <niemann@ecegrid-thin1.ecn.purdue.edu>
2014-10-17 18:27:08 -0400

    partial

--- PA06/answer06.c
+++ PA06/answer06.c
@@ -68,7 +68,7 @@ void traverse (char * * maze, int w, int h, int x, int y, int movex, int movey)
   while (maze[x][y] != 'X')
     {
       //steps++;
-      maze[x][y] = 'x';
+      //maze[x][y] = 'x';
       
       if (x + movex < h && x >= 0)
 	{
@@ -80,7 +80,7 @@ void traverse (char * * maze, int w, int h, int x, int y, int movex, int movey)
 	  y += movey;
 	}
       
-      //maze[x][y] = 'x';
+      maze[x][y] = 'x';
       if (y + 1 < w && y + 1 >= 0)
 	{
 	  right = (maze[x][y + 1] == ' ');
@@ -173,13 +173,142 @@ void print_directions (char * * maze, int w, int h)
 int main(int argc, char * * argv)
 {
   //LOCAL DECLARATIONS
-  print_path(-1,0,1);
+  //print_path(-1,0,1);
 
   //EXECUTABLE STATEMENTS
   
 
   return EXIT_SUCCESS;
 }
-  //inside recursive while you keep going
+  //inside recursive 
+  //move(s)
+  //for(1-4)  //directions
+  //Recurse
+  //move(n)
   
-  */
+
+
+int canTraverse(char * * maze, int h, int w, int row, int col, int direction)
+{
+  //LOCAL DECLARATIONS
+  int can = 0;
+  int dr = 0;
+  int dc = 0;
+  
+  if (direction == NORTH)
+    {
+      dr = -1;
+      dc = 0;
+    }
+  else if (direction == SOUTH)
+    {
+      dr = 1;
+      dc = 0;
+    }
+  else if (direction == WEST)
+    {
+      dr = 0;
+      dc = -1;
+    }
+  else if (direction == EAST)
+    {
+      dr = 0;
+      dc = 1;
+    }
+
+  //EXECUTABLE STATEMENTS
+  if (maze[row + dr][col + dc] == ' ' && row + dr >= 0 && row <= h && col + dc >= 0 && col + dc <= w)
+    {
+      can = 1;
+    }
+  
+  return can;
+}
+
+void traverse(char * * maze, int direction, int * row, int * col)
+{
+  //LOCAL DECLARATIONS
+  char dir;
+  if (direction == NORTH)
+    {
+      dir = 'N';
+    }
+  else if (direction == SOUTH)
+    {
+      dir = 'S';
+    }
+  else if (direction == EAST)
+    {
+      dir = 'E';
+    }
+  else if (direction == WEST)
+    {
+      dir = 'W';
+    }
+ 
+  //EXECUTABLE STATEMENTS
+  printf("%c %d\n", dir, 1);
+ 
+  if (direction == NORTH)
+    {
+      maze[*row][*col] = '.';
+      *row += -1;
+      *col += 0;
+    }
+  else if (direction == SOUTH)
+    {
+      maze[*row][*col] = '.';
+      *row += 1;
+      *col += 0;
+    }
+  else if (direction == EAST)
+    {
+      maze[*row][*col] = '.';
+      *row += 0;
+      *col += 1;
+    }
+  else if (direction == WEST)
+    {
+      maze[*row][*col] = '.';
+      *row += 0;
+      *col += -1;
+    }
+
+  return;
+}
+
+void print_directions (char * * maze, int w, int h)                                                                                          
+{                                                                                                                                            
+  //LOCAL DECLARATIONS                                                       
+  int col = starting_point(maze);                                                                                                   
+  int row = 0;                                                                                                                            
+                                                                                                                      
+  //EXECUTABLE STATEMENTS                                                                                      
+  traverse(maze,SOUTH,&row,&col);                                                                                              
+                                                                                                                                               
+  return;  
+} 
+
+void recur(char * * maze, int h, int w, int row, int col)
+{
+  //LOCAL DECLARATIONS
+  //int stpt = starting_point(maze);
+  int directions[] = {NORTH, SOUTH, EAST, WEST};
+  int ind;
+
+  //EXECUTABLE STATEMENTS
+  traverse(maze,SOUTH,&row,&col);
+  
+  for (ind = 0; ind < 4; ind++)
+    {
+      if (canTraverse(maze,h,w,row,col,directions[ind]))
+	{
+	  recur(maze,h,w,row,col);
+	}
+      
+    }
+  
+  traverse(maze,NORTH,&row,&col);
+
+  return;
+}*/
