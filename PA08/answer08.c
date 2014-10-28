72f4f98efe5e32a81501a596c656afffc9b818a0
Caleb Paul Niemann <niemann@ecegrid-thin1.ecn.purdue.edu>
2014-10-27 22:11:54 -0400

    create,destroy complete

--- PA08/answer08.c
+++ PA08/answer08.c
@@ -1,3 +1,4 @@
+#include"answer08.h"
 #include<string.h>
 #include<stdio.h>
 #include<stdlib.h>
@@ -5,4 +6,54 @@
 
 List * List_createNode(const char * str)
 {
+  //LOCAL DECLARATIONS 
+  List * head = NULL;
+  head = malloc(sizeof(List));
+
+  //EXECUTABLE STATEMENTS
+  head->next = NULL;
+  head->str = strdup(str);
+
+  return head;
+}
+
+void List_destroy(List * list)
+{
+  //LOCAL DECLARATIONS
+
+  //EXECUTABLE STATEMENTS
+  if (list == NULL)
+    {
+      return;
+    }
+  
+  List_destroy(list->next);
+  free(list);
+
+  return;
+}
+
+int List_length(List * list)
+{
+  //EXECUTABLE STATEMENTS
+  int length = 0;
+  
+  //LOCAL DECLARATIONS
+  while (list->next != NULL)
+    {
+      length++;
+      list = list->next;
+    } 
+
+  return length;
+}
+
+List * List_merge(List * lhs, List * rhs, int (*compar)(const char *, const char*))
+{
+  return lhs;
+}
+
+List * List_sort(List * list, int (*compar)(const char *, const char*))
+{
+  return list;
 }
