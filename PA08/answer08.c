cb63b255d98356e59a95e0a96627ea6a0279475d
Caleb Paul Niemann <niemann@ecegrid-thin4.ecn.purdue.edu>
2014-10-29 18:24:56 -0400

    3/5

--- PA08/answer08.c
+++ PA08/answer08.c
@@ -17,28 +17,43 @@ List * List_createNode(const char * str)
   return head;
 }
 
+void destruct(List * list)
+{
+  if (list != NULL)
+    {
+      free(list->str);
+      free(list);
+    }
+
+  return;
+}
+
 void List_destroy(List * list)
 {
   //LOCAL DECLARATIONS
 
   //EXECUTABLE STATEMENTS
-  if (list == NULL)
+  while (list != NULL)
     {
-      return;
-    }
-  
-  List_destroy(list->next);
-  free(list);
+      List * next = list->next;
+      destruct(list);
+      list = next;
+    } 
 
-  return;
+      return;
 }
 
 int List_length(List * list)
 {
-  //EXECUTABLE STATEMENTS
-  int length = 0;
-  
   //LOCAL DECLARATIONS
+  int length = 1;
+  
+  //EXECUTABLE STATEMENTS
+  if (list == NULL)
+    {
+      return 0;
+    }
+
   while (list->next != NULL)
     {
       length++;
@@ -48,9 +63,80 @@ int List_length(List * list)
   return length;
 }
 
+List * List_traverse(List * head)
+{
+  //LOCAL DECLARATIONS
+  List * ptr = head;
+
+  //EXECUTABLE STATEMENTS
+  while (ptr != NULL)
+    {
+      if (ptr->next == NULL)
+	{
+	  return ptr;
+	}
+    }
+
+  return ptr;
+}
+
+void insert(List * dest, List * src)
+{
+  //LOCAL DECLARATIONS
+  
+  //EXECUTABLE STATEMENTS
+  dest = List_traverse(dest);
+
+  while (src != NULL)
+    {
+      dest->next = src;
+      src = src->next;
+    }
+  
+  dest->next = NULL;
+
+  return;
+}
 List * List_merge(List * lhs, List * rhs, int (*compar)(const char *, const char*))
 {
-  return lhs;
+  //LOCAL DECLARATIONS
+  //int bigger = compar(lhs->str, rhs->str);
+  List * new = NULL;
+  //new->next = NULL;
+  //List * tmp = malloc(sizeof(List));
+  //new->next = NULL;
+
+  //EXECUTABLE STATEMENTS
+  while (lhs != NULL && rhs != NULL)
+    {
+      int bigger = compar(lhs->str,rhs->str);
+
+      if (bigger <= 0)
+	{
+	  tmp = lhs;
+	  lhs = lhs->next;
+	  tmp->next = NULL;
+	  insert(new,tmp);
+	}
+      else
+	{
+	  tmp = rhs;
+	  tmp->next = NULL;
+	  rhs = rhs->next;
+	  insert(new,tmp);
+	}
+    }
+
+  if (lhs != NULL)
+    {
+      insert(new,lhs);
+    }
+  else if (rhs != NULL)
+    {
+      insert(new,rhs);
+    }
+
+  return new;
 }
 
 List * List_sort(List * list, int (*compar)(const char *, const char*))
